1257b87448527077a85697db24dfa50616e5d1ab
Eetu Sutinen <eetu.sutinen@gmail.com>
2015-11-18 20:28:45 +0200

    use store for storing state

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 import React from 'react';
-import Playground from './components/playground';
-import ReactDOM from 'react-dom';
+import Playground from './containers/playground';
+import {render} from 'react-dom';
+import {Provider} from 'react-redux';
+import configureStore from './store/configureStore';
 
-ReactDOM.render(<Playground />, document.getElementById('app'));
+const store = configureStore();
+
+render(
+  <Provider store={store}>
+    <Playground />
+  </Provider>,
+  document.getElementById('app')
+);
