e63de1c98b04a92ae18f6e7903ebecee85a7dbfa
Eetu Sutinen <eetu.sutinen@gmail.com>
2015-12-06 19:11:49 +0200

    extend Component class

--- src/components/input/radio-field/index.jsx
+++ src/components/input/radio-field/index.jsx
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-export default React.createClass({
+class RadioField extends Component {
   onValueChange(event) {
     this.props.onChange(event.target.value);
-  },
+  }
+
   render() {
     return (
       <div className='radio-field'>
-        <input type='radio' name='type' value={this.props.text} onChange={this.onValueChange}/>
+        <input type='radio' name='type' value={this.props.text}
+          onChange={this.onValueChange.bind(this)}/>
         <label>{this.props.text}</label>
       </div>
     );
   }
-});
+}
+
+export default RadioField;
--- src/components/rangeSlider/index.jsx
+++ src/components/rangeSlider/index.jsx
@@ -1,33 +1,39 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-export default React.createClass({
-  getInitialState() {
-    return {value: 300};
-  },
+class RangeSlider extends Component {
+  value = 300;
+
   handleValueChange(event) {
-    let value = event.target.value;
+    const value = event.target.value;
     this.setState({value: value});
     this.props.onValueChanged(value);
-  },
+  }
+
   handleMouseWheel(event) {
     event.preventDefault();
 
-    let newValue = this.state.value + event.deltaY * this.props.step;
-    if (newValue < this.props.min) {
+    let newValue = this.value + event.deltaY * this.props.step;
+    if(newValue < this.props.min) {
       newValue = this.props.min;
-    } else if (newValue > this.props.max) {
+    } else if(newValue > this.props.max) {
       newValue = this.props.max;
     }
-    this.setState({value: newValue});
+    this.value = newValue;
     this.props.onValueChanged(newValue);
-  },
+  }
+
   render() {
     return (
-      <div className="range-slider" onWheel={this.handleMouseWheel}>
-        <input type="range" className="range-slider__slider" min={this.props.min} orient="vertical" value={this.state.value} max={this.props.max} step={this.props.step} onChange={this.handleValueChange}/>
+      <div className='range-slider' onWheel={this.handleMouseWheel.bind(this)}>
+        <input type='range' className='range-slider__slider' min={this.props.min}
+          orient='vertical' value={this.value} max={this.props.max} step={this.props.step}
+          onChange={this.handleValueChange}/>
         <br/>
-        <input type="text" className="range-slider__input" value={this.state.value} onChange={this.handleValueChange} />
+        <input type='text' className='range-slider__input' value={this.value}
+          onChange={this.handleValueChange}/>
       </div>
     );
   }
-});
+}
+
+export default RangeSlider;
