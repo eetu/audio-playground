795cf87b2f0ff641d59449758905cac95b66f911
Eetu Sutinen <eetu.sutinen@gmail.com>
2015-12-09 00:13:16 +0200

    use freq helper to play notes

--- src/components/keyboard/index.jsx
+++ src/components/keyboard/index.jsx
@@ -1,6 +1,6 @@
 import React, {Component, PropTypes} from 'react';
 import _ from 'lodash';
-import * as Notes from '../../constants/notes';
+import {frequency} from '../../lib/helper.js';
 
 class Keyboard extends Component {
   componentWillMount() {
@@ -8,47 +8,64 @@ class Keyboard extends Component {
     document.addEventListener('keyup', this.handleKeyUp.bind(this), false);
   }
 
-  keysPressed = {}
+  keysPressed = {};
+
+  octave = 5;
 
   handleKeyDown(event) {
     const key = event.keyCode;
+    if(key >= 48 && key <= 57) {
+      this.octave = key - 48;
+    }
     this.keysPressed[key] = true;
     switch(key) {
     case 65:
-      this.props.actions.playNote(Notes.C5);
+      this.props.actions.playNote(frequency('C' + this.octave));
       break;
     case 87:
-      this.props.actions.playNote(Notes.Cb5);
+      this.props.actions.playNote(frequency('C#' + this.octave));
       break;
     case 83:
-      this.props.actions.playNote(Notes.D5);
+      this.props.actions.playNote(frequency('D' + this.octave));
       break;
     case 69:
-      this.props.actions.playNote(Notes.Db5);
+      this.props.actions.playNote(frequency('D#' + this.octave));
       break;
     case 68:
-      this.props.actions.playNote(Notes.E5);
+      this.props.actions.playNote(frequency('E' + this.octave));
       break;
     case 70:
-      this.props.actions.playNote(Notes.F5);
+      this.props.actions.playNote(frequency('F' + this.octave));
       break;
     case 84:
-      this.props.actions.playNote(Notes.Fb5);
+      this.props.actions.playNote(frequency('F#' + this.octave));
       break;
     case 71:
-      this.props.actions.playNote(Notes.G5);
+      this.props.actions.playNote(frequency('G' + this.octave));
       break;
     case 89:
-      this.props.actions.playNote(Notes.Gb5);
+      this.props.actions.playNote(frequency('G#' + this.octave));
       break;
     case 72:
-      this.props.actions.playNote(Notes.A5);
+      this.props.actions.playNote(frequency('A' + this.octave));
       break;
     case 85:
-      this.props.actions.playNote(Notes.Ab5);
+      this.props.actions.playNote(frequency('A#' + this.octave));
       break;
     case 74:
-      this.props.actions.playNote(Notes.B5);
+      this.props.actions.playNote(frequency('B' + this.octave));
+      break;
+    case 75:
+      this.props.actions.playNote(frequency('C' + (this.octave + 1)));
+      break;
+    case 79:
+      this.props.actions.playNote(frequency('C#' + (this.octave + 1)));
+      break;
+    case 76:
+      this.props.actions.playNote(frequency('D' + (this.octave + 1)));
+      break;
+    case 80:
+      this.props.actions.playNote(frequency('D#' + (this.octave + 1)));
       break;
     default:
       //
@@ -65,7 +82,7 @@ class Keyboard extends Component {
 
   render() {
     return (
-      <div>[A S D F G H J]</div>
+      <div>[add osc, and play your asdasdasd]</div>
     );
   }
 }
