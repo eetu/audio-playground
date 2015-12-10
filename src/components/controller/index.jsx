17c699d70acbe49102dee79c0126aa2bae17adc2
Eetu Sutinen <eetu.sutinen@gmail.com>
2015-12-11 00:09:34 +0200

    remove add osc button

--- src/components/controller/index.jsx
+++ src/components/controller/index.jsx
@@ -6,17 +6,11 @@ class Controller extends Component {
     super(props, context);
   }
 
-  handleOscillatorAdd() {
-    this.props.actions.addOscillator();
-  }
-
   render() {
     const {oscillators, actions, audioContext} = this.props;
 
     return (
       <div className='controller'>
-        <button className='btn btn--action' onClick={this.handleOscillatorAdd.bind(this)}>Add Oscillator</button>
-
         <div>
           {oscillators.map(oscillator =>
             <Oscillator key={oscillator.id} actions={actions} audioContext={audioContext} oscillator={oscillator}/>
