26f33a0edde664d87d842507b02d869fcf7fac26
Eetu Sutinen <eetu.sutinen@gmail.com>
2015-12-13 19:57:17 +0200

    support hot reloading reducers

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,7 +1,21 @@
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
 
+const createStoreWithMiddleware = applyMiddleware(
+  thunk
+)(createStore);
+
 export default function configureStore(initialState) {
-  const store = createStore(rootReducer, initialState);
+  const store = createStoreWithMiddleware(rootReducer, initialState);
+
+  if(module.onReload) {
+    module.onReload(() => {
+      const nextReducer = require('../reducers');
+      store.replaceReducer(nextReducer.default || nextReducer);
+      return true;
+    });
+  }
+
   return store;
 }
