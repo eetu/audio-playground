71f6b49958358af44333687d29f513c97adbaa00
Eetu Sutinen <eetu.sutinen@gmail.com>
2016-09-17 15:31:59 +0300

    change switch color depending on witch state

--- src/components/input/flip-switch/index.jsx
+++ src/components/input/flip-switch/index.jsx
@@ -6,12 +6,13 @@ class FlipSwitch extends Component {
   }
 
   render() {
+    const borderStyle = `flip-switch__switch-border ${this.props.checked ? 'on' : 'off'}`;
     return (
       <div className='flip-switch'>
         <label className='flip-switch__title'>{this.props.text}</label>
         <label className='flip-switch__left-label' htmlFor={this.props.text}>{this.props.offText}</label>
         <input className='flip-switch__checkbox' type='checkbox' id='foobar' onChange={this.onValueChange.bind(this)} defaultChecked={this.props.checked}/>
-        <label className='flip-switch__switch-border' htmlFor='foobar'>
+        <label className={borderStyle} htmlFor='foobar'>
           <span className='flip-switch__switch'></span>
         </label>
         <label className='flip-switch__right-label' htmlFor={this.props.text}>{this.props.onText}</label>
